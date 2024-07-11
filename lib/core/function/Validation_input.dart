233849f0df323eb2e37a2f21f36dcdb39b0a25a1
seleena <seleena.shomali@pcnc2000.com>
2024-07-11 10:23:51 +0300

    lenght validation

--- lib/core/function/Validation_input.dart
+++ lib/core/function/Validation_input.dart
@@ -5,28 +5,32 @@ String? validInput(String val, String field) {
   if (val.isEmpty) {
     return "$field can't be empty";
   } else if (field == 'Email') {
-    if (!val.contains('@')) {
+    if (val.length > 100) {
+      return 'Email must be less than or equal to 100 characters';
+    } else if (!val.contains('@')) {
       return 'A valid email address is required';
     } else if (RegExp(dangerousCharacters).hasMatch(val)) {
       return "Email contains dangerous characters";
     }
   } else if (field == 'Username') {
     if (val.length < 6 || val.length > 100) {
-      return 'The username must be between 6 and 100 characters long.';
+      return 'Username must be between 6 and 100 characters long';
     } else if (RegExp(dangerousCharacters).hasMatch(val)) {
       return "Username contains dangerous characters";
     }
   } else if (field == 'Password') {
     if (val.length < 8) {
-      return "$field must be at least 8 characters";
+      return 'Password must be at least 8 characters';
+    } else if (val.length > 100) {
+      return 'Password must be less than or equal to 100 characters';
     } else if (!RegExp(r'(?=.*[a-z])').hasMatch(val)) {
-      return "$field must contain at least one lowercase letter";
+      return 'Password must contain at least one lowercase letter';
     } else if (!RegExp(r'(?=.*[A-Z])').hasMatch(val)) {
-      return "$field must contain at least one uppercase letter";
+      return 'Password must contain at least one uppercase letter';
     } else if (!RegExp(r'(?=.*\d)').hasMatch(val)) {
-      return "$field must contain at least one digit";
+      return 'Password must contain at least one digit';
     } else if (!RegExp(r'(?=.*[!@#$%^&*()_+=|<>?{}[\]~-])').hasMatch(val)) {
-      return "$field must contain at least one special character";
+      return 'Password must contain at least one special character';
     } else if (RegExp(dangerousCharacters).hasMatch(val)) {
       return "Password contains dangerous characters";
     }
