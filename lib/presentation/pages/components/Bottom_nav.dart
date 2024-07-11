16d02b39d056c2918e11d4254b46ed9f31e35378
seleena <seleena.shomali@pcnc2000.com>
2024-07-11 13:48:40 +0300

    homesvg

--- lib/presentation/pages/components/Bottom_nav.dart
+++ lib/presentation/pages/components/Bottom_nav.dart
@@ -1,23 +1,26 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       backgroundColor: Colors.black,
-      selectedItemColor: Colors.black,
+      selectedItemColor: Colors.red,
       unselectedItemColor: Colors.black,
       showSelectedLabels: true,
       showUnselectedLabels: true,
       items: [
         BottomNavigationBarItem(
-          icon: Icon(
-            Icons.home_outlined,
-            color: Colors.black,
-          ),
-          label: 'Home',
+
+          icon: SizedBox(
+            height: 22,
+            width: 20,
+            child: Image.asset('assets/images/Hvector.png')),
+          label: 'Home' ,
+          
         ),
         BottomNavigationBarItem(
           icon: Icon(
